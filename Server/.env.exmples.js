338f8a39328a33e92614c7ecd3345e5b5a48fcc6
Qasim Farooqi <148307072+qasimf10@users.noreply.github.com>
2026-01-03 11:59:35 -0800

    Remove sensitive Cloudinary credentials
    Cleared Cloudinary configuration values for security.

--- Server/.env.exmples.js
+++ Server/.env.exmples.js
@@ -2,7 +2,7 @@
 MONGO_URI="mongodb+srv://qasimfarooqi09_db_user:7RHUjNzyvU0wr07h@cluster0.z3aoi8e.mongodb.net/?appName=Cluster0"
 
 # Cloudinary
-CLOUDINARY_CLOUD_NAME="du5tamjvq"
-CLOUDINARY_API_KEY="436371915728456"
-CLOUDINARY_API_SECRET="KpGwuhhcwfxVFJ9GSlQypTyeWHo"
-CLOUDINARY_URL="cloudinary://436371915728456:KpGwuhhcwfxVFJ9GSlQypTyeWHo@du5tamjvq"
+CLOUDINARY_CLOUD_NAME=""
+CLOUDINARY_API_KEY=""
+CLOUDINARY_API_SECRET=""
+CLOUDINARY_URL=""
